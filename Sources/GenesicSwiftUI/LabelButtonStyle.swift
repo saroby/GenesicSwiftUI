837a32aff72578e420254d77ac6bc80a969956cd
Heewook <saroby@naver.com>
2024-11-25 22:55:51 +0900

    LabelButtonStyle 추가

--- Sources/GenesicSwiftUI/LabelButtonStyle.swift
+++ Sources/GenesicSwiftUI/LabelButtonStyle.swift
@@ -2,6 +2,9 @@ import SwiftUI
 
 // default pressed effect를 제거하기 위함
 public struct LabelButtonStyle: ButtonStyle {
+    
+    public init() {}
+    
     public func makeBody(configuration: Configuration) -> some View {
         configuration
             .label
